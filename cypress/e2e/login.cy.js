cd3228251372bf7ba78d9b4df7affce193952d22
Marilia Vieira <mariliavv@gmail.com>
2026-08-08 19:41:27 -0300

    Adicionei um teste de login invalido

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,5 @@
 describe('template spec', () => {
-  it('passes', () => {
+  it('Login com dados válidos deve permitir entrada no sistema', () => {
     //Arrange
     cy.visit('http://localhost:4000')
     //Act
@@ -8,5 +8,16 @@ describe('template spec', () => {
     cy.get('#login-section > .btn').click()
     //Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
+  })
+
+  it('Login com dados inválidos deve apresentar mensagem de erro', () => {
+    //Arrange
+    cy.visit('http://localhost:4000')
+    //Act
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('654321')
+    cy.contains('button', 'Entrar').click()
+    //Assert
+    cy.get('.toast').should('be.visible').should('have.text', 'Erro no login. Tente novamente.')
   })
 })
