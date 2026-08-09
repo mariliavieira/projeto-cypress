ea17ffbfe60c96c80611a1f548c34a09fad247a9
Marilia Vieira <mariliavv@gmail.com>
2026-08-08 21:26:37 -0300

    Adicionei o before-each para os testes de logim

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,9 @@
 describe('template spec', () => {
-  it('Login com dados válidos deve permitir entrada no sistema', () => {
+  beforeEach(() => {
     //Arrange
     cy.visit('http://localhost:4000')
+  })
+  it('Login com dados válidos deve permitir entrada no sistema', () => {
     //Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
@@ -11,8 +13,6 @@ describe('template spec', () => {
   })
 
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
-    //Arrange
-    cy.visit('http://localhost:4000')
     //Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('654321')
