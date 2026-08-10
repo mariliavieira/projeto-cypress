061a82bbe66f70eec308c5d258529e41f6b7da9a
Marilia Vieira <mariliavv@gmail.com>
2026-08-10 13:07:20 -0300

    Adicionando a variavel URL de ambiente

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,7 @@
 describe('template spec', () => {
   beforeEach(() => {
     //Arrange
-    cy.visit('http://localhost:4000')
+    cy.visit(Cypress.env('URL'))
   })
   it('Login com dados válidos deve permitir entrada no sistema', () => {
     //Act
