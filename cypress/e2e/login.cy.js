9760201ce7ca43e3f5054fb4b82b9384af54905c
Marilia Vieira <mariliavv@gmail.com>
2026-08-11 11:47:40 -0300

    adcionando o base url do cypress

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,7 @@
 describe('template spec', () => {
   beforeEach(() => {
     //Arrange
-    cy.visit(Cypress.env('URL'))
+    cy.visit('/')
   })
   it('Login com dados válidos deve permitir entrada no sistema', () => {
     //Act
