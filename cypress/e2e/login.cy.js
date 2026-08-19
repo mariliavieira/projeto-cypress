2e66f022afe5bd4869b51a7634bc00377acb0ef6
Marilia Vieira <mariliavv@gmail.com>
2026-08-18 22:31:31 -0300

    implementando comandos customizados

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,23 +5,15 @@ describe('template spec', () => {
   })
   it('Login com dados válidos deve permitir entrada no sistema', () => {
     //Act
-    cy.fixture('credenciais').then(credenciais => {
-      cy.get('#username').click().type(credenciais.valida.usuario)
-      cy.get('#senha').click().type(credenciais.valida.senha)
-    })
-    cy.get('#login-section > .btn').click()
+    cy.fazerLoginCredenciaisValidas()
     //Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
 
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
     //Act
-    cy.fixture('credenciais').then(credenciais => {
-      cy.get('#username').click().type(credenciais.invalida.usuario)
-      cy.get('#senha').click().type(credenciais.invalida.senha)
-    })
-    cy.contains('button', 'Entrar').click()
+    cy.fazerLoginCredenciaisInvalidas()
     //Assert
-    cy.get('.toast').should('be.visible').should('have.text', 'Erro no login. Tente novamente.')
+    cy.verificarMensagemNoToast('Erro no login. Tente novamente.')
   })
 })
